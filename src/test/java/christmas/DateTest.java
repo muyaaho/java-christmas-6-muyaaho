bce7273c06a3aff8430e31d6fb299a7405bd03c1
muyaaho <skanthsu715@gmail.com>
2023-11-11 14:50:46 +0900

    refactor(christmas): 중복되는 코드를 ParameterizedTest 로 수정

--- src/test/java/christmas/DateTest.java
+++ src/test/java/christmas/DateTest.java
@@ -2,33 +2,32 @@ package christmas;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class DateTest {
 
-    @Test
-    void 날짜_객체에_저장(){
-        ReservationDate reservationDate = new ReservationDate("4");
-        assertEquals(4, reservationDate.getDay());
-
-        ReservationDate reservationDate2 = new ReservationDate("31");
-        assertEquals(31, reservationDate2.getDay());
+    @ParameterizedTest
+    @ValueSource(strings = {"4", "1", "31"})
+    void 날짜는_1_이상_31_이하(String day){
+        assertDoesNotThrow(() -> new ReservationDate(day));
     }
 
-    @Test
-    void 날짜는_1_이상_31_이하(){
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("41"));
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("0"));
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("-1")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    @ParameterizedTest
+    @ValueSource(strings = {"41", "0", "-1", "32"})
+    void 날짜는_1_이상_31_이하가_아닌_숫자들(String day){
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate(day)).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 
-    @Test
-    void 날짜에_문자가_들어올때_예외처리(){
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("a123")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("aaaaaa")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
-        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate(".")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    @ParameterizedTest
+    @ValueSource(strings = {"a1234", "aaaa", "."})
+    void 날짜에_문자가_들어올때_예외처리(String input){
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate(input)).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 
 }
