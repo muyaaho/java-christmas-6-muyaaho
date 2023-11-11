84e0cfd03addf660e4f42b048c804787d12feafd
muyaaho <skanthsu715@gmail.com>
2023-11-11 14:39:55 +0900

    feat(christmas): 문자가 들어왔을 때 예외 처리 추가

--- src/main/java/christmas/ReservationDate.java
+++ src/main/java/christmas/ReservationDate.java
@@ -6,8 +6,9 @@ public class ReservationDate {
     private int day;
 
     public ReservationDate(String day) {
+        validateDay(day);
         int convertDay = parseInt(day);
-        oneToThirtyOne(convertDay);
+        validateOneToThirtyOne(convertDay);
         this.day = convertDay;
     }
 
@@ -15,8 +16,16 @@ public class ReservationDate {
         return day;
     }
 
-    private void oneToThirtyOne(int day){
+    private void validateOneToThirtyOne(int day){
         if (day < 1 || day > 31) {
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateDay(String day){
+        try{
+            parseInt(day);
+        } catch(NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
     }
--- src/test/java/christmas/DateTest.java
+++ src/test/java/christmas/DateTest.java
@@ -21,7 +21,14 @@ public class DateTest {
     void 날짜는_1_이상_31_이하(){
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("41"));
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("0"));
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("-1")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
     }
 
+    @Test
+    void 날짜에_문자가_들어올때_예외처리(){
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("a123")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate("aaaaaa")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new ReservationDate(".")).withMessage("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
 
 }
