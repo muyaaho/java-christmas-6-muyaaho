046930135246f7c6d1e6862a88ed146f41bacb09
muyaaho <skanthsu715@gmail.com>
2023-11-11 17:41:42 +0900

    feat(christmas): 한 줄 주문 입력이 들어왔을 때 생길 수 있는 예외 처리 추가

--- src/main/java/christmas/InputController.java
+++ src/main/java/christmas/InputController.java
@@ -4,39 +4,58 @@ import static java.lang.Integer.parseInt;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.PatternSyntaxException;
-import net.bytebuddy.pool.TypePool.Resolution.Illegal;
+import java.util.stream.Collectors;
+
 
 public class InputController {
 
     public List<String> seperateComma(String input){
-        // 리스트 필터 돌려서 공백과 콤마 다 잡아내고 사이즈 비교해서 다르면 에러
         List<String> seperated = Arrays.asList(input.split(",", -1));
-        int blankCount = (int)seperated.stream().filter(e -> e.isBlank()).count();
-        if (blankCount>0){
-            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-        }
+        validateBlank(seperated);
+        seperated.stream().forEach(s -> {
+            validateNotHypen(s);
+            validateNumber(s);
+            validateElementBlank(s);
+        });
+
         return seperated;
     }
 
 
     public WootecoMenu inputToWootechMenu(String seperatedInput){
+        validateNotHypen(seperatedInput);
+        validateNumber(seperatedInput);
         String[] splitInput = seperatedInput.split("-");
-        validateNotHypen(splitInput);
-        validateNumber(splitInput[1]);
         return new WootecoMenu(splitInput[0], parseInt(splitInput[1]));
     }
 
-    private void validateNotHypen(String[] splitInput){
-        if (splitInput.length < 2){
+    private void validateElementBlank(String element){
+        String[] splitInput = element.split("-");
+        if (splitInput[0].isBlank() || splitInput[1].isBlank()){
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
 
-    private void validateNumber(String number){
+    private void validateNotHypen(String seperatedInput){
+        String[] splitInput = seperatedInput.split("-");
+        System.out.println(Arrays.toString(splitInput));
+        if (splitInput.length != 2){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateNumber(String seperatedInput){
+        String[] splitInput = seperatedInput.split("-");
         try{
-            parseInt(number);
+            parseInt(splitInput[1]);
         } catch (NumberFormatException e){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+    }
+
+    private void validateBlank(List<String> seperated) {
+        int blankCount = (int)seperated.stream().filter(e -> e.isBlank()).count();
+        if (blankCount>0){
             throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
         }
     }
