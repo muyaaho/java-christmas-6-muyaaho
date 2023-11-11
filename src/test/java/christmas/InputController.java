f6770ba44061c144cc3579089d8867c5256378d7
muyaaho <skanthsu715@gmail.com>
2023-11-11 16:06:28 +0900

    feat(christmas): 메뉴 입력할 때 하이픈이 없는 경우 에외 처리 추가

--- src/test/java/christmas/InputController.java
+++ src/test/java/christmas/InputController.java
@@ -2,6 +2,9 @@ package christmas;
 
 import static java.lang.Integer.parseInt;
 
+import java.util.regex.PatternSyntaxException;
+import net.bytebuddy.pool.TypePool.Resolution.Illegal;
+
 public class InputController {
 
     public void checkHyhpen(String input){
@@ -10,7 +13,14 @@ public class InputController {
 
     public WootecoMenu inputToWootechMenu(String input){
         String[] splitInput = input.split("-");
+        validateNotHypen(splitInput);
         return new WootecoMenu(splitInput[0], parseInt(splitInput[1]));
+    }
+
+    private void validateNotHypen(String[] splitInput){
+        if (splitInput.length < 2){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
     }
 
 
--- src/test/java/christmas/InputControllerTest.java
+++ src/test/java/christmas/InputControllerTest.java
@@ -1,18 +1,26 @@
 package christmas;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InputControllerTest {
+    InputController inputController;
+    @BeforeEach
+    void setUp(){
+        inputController = new InputController();
+    }
     @Test
     void 메뉴_입력을_WootecoMenu객체로(){
-        InputController inputController = new InputController();
         assertThat(new WootecoMenu("티본스테이크", 1)).isEqualToComparingFieldByField(inputController.inputToWootechMenu("티본스테이크-1"));
     }
-//    @Test
-//    void 메뉴_형식이_예시와_다른_경우(){
-//        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new InputContoller.checkHyhpen("티본스테이크1")).withMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
-//    }
+    @Test
+    void 메뉴입력에_하이픈이_없는_경우(){
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> inputController.inputToWootechMenu("티본스테이크1")).withMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+    }
+
+
 }
