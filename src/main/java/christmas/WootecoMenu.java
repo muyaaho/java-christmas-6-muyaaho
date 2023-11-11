766d6f0e7523388afdedf9b9442e3980c545f147
muyaaho <skanthsu715@gmail.com>
2023-11-11 15:29:21 +0900

    feat(christmas): 메뉴판에 없는 메뉴 예외 처리 추가

--- src/main/java/christmas/WootecoMenu.java
+++ src/main/java/christmas/WootecoMenu.java
@@ -1,10 +1,18 @@
 package christmas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class WootecoMenu {
     private String name;
     private int count;
-
+    List<String> menuboard = new ArrayList<>(Arrays.asList("양송이수프", "타파스", "시저샐러드", "티본스테이크", "바비큐립", "해산물파스타", "크리스마스파스타", "초코케이크", "아이스크림", "제로콜라", "레드와인", "샴페인"));
     public WootecoMenu(String name, int count) {
+        if (!menuboard.contains(name)){
+            throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        }
+
         this.name = name;
         this.count = count;
     }
--- src/test/java/christmas/ValidateMenuTest.java
+++ src/test/java/christmas/ValidateMenuTest.java
@@ -1,13 +1,23 @@
 package christmas;
 
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidateMenuTest {
 
     @Test
     void 메뉴에_있는_음식이입력되면_통과(){
         assertDoesNotThrow(() -> new WootecoMenu("티본스테이크", 1));
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"마라탕", "닭볶음탕", "삼겹살구이", "초밥", "맥주"})
+    void 메뉴판에_없는_메뉴_주문(String foodName){
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new WootecoMenu(foodName,1)).withMessage("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
     }
 }
