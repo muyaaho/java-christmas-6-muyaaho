605cebfe523f673561aed8fa7d9173449c0ab584
muyaaho <skanthsu715@gmail.com>
2023-11-15 21:58:19 +0900

    refactor(christmas): 음료 예외 삭제하고 모두 유효하지 않은 주문 메시지로 변경

--- src/main/java/christmas/Exception/DrinkException.java
+++
@@ -1,7 +0,0 @@
-package christmas.Exception;
-
-public class DrinkException extends IllegalArgumentException{
-    public DrinkException() {
-        super("[ERROR] 음료만 주문할 수 없습니다. 다시 입력해 주세요");
-    }
-}
