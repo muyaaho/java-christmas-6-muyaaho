e001b685ec1f289580b762723ffaa3ac7e55ccfc
muyaaho <skanthsu715@gmail.com>
2023-11-30 14:25:43 +0900

    refactor(christmas): 바베큐립 오타 수정

--- src/main/java/christmas/domain/MenuBoard.java
+++ src/main/java/christmas/domain/MenuBoard.java
@@ -1,7 +1,6 @@
 package christmas.domain;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
 
 public enum MenuBoard {
@@ -9,7 +8,7 @@ public enum MenuBoard {
     TAPAS("타파스",5_500, "APPETIZER"),
     CAESAR_SALAD("시저샐러드", 8_000, "APPETIZER"),
     T_BONE_STEAK("티본스테이크", 55_000, "MAIN"),
-    BARBECUE_LIP("바비큐립",54_000, "MAIN"),
+    BARBECUE_RIP("바비큐립",54_000, "MAIN"),
     SEAFOOD_PASTA("해산물파스타", 35_000, "MAIN"),
     CHRISTMAS_PASTA("크리스마스파스타", 25_000, "MAIN"),
     CHOCOLATE_CAKE("초코케이크", 15_000, "DESSERT"),
